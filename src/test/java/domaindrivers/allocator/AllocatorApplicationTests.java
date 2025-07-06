bc4ccb29b1bf4bc8bae1ec5ae080245250e1668e
Łukasz Duda <lukasz.duda.pl@gmail.com>
2025-07-06 13:37:26 +0200

    Initialize

---
+++ src/test/java/domaindrivers/allocator/AllocatorApplicationTests.java
@@ -0,0 +1,13 @@
+package domaindrivers.allocator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllocatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
